2b9f8847bec61193b50aac2be08383844da4995e
dhanuskaasri <25bcs016@kgcas.com>
2025-09-14 21:07:37 +0530

    c-programs

---
+++ data type task2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	printf("Size of a char: %zu byte\n",sizeof(char));
+	printf("size of an int:%zu bytes\n",sizeof(int));
+	printf("size of a float:%zu bytes\n",sizeof(float));
+	printf("size of a double:%zu bytes\n",sizeof(double));
+	
+	
+return 0;
+
+
+}
---
+++ data types taks1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	//Declare and initialize variables
+	int int_variable=10;
+	float float_variable=3.14;
+	char char_variable='A';
+	double double_variable=123.456789;
+	
+	
+	//print the variables using format specifiers
+	printf("Integer variables:%d\n",int_variable);
+	printf("float variable:%f\n",float_variable);
+	printf("character variable:%c\n",char_variable);
+	printf("Double variable:%1f\n",double_variable);
+	
+	
+	return 0;
+}
